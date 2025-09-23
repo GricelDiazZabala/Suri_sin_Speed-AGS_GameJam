097f2023066a6498c6047f8275033b9aa77510b9
Gricel Diaz Zabala <griceldiazzabala@gmail.com>
2025-09-23 18:53:09 -0300

    no se muestra el fondo

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
 import pygame
+pygame.init()
 import sys
 import os
-import time
-import random
+import spritesheet
 pygame.font.init()
+pygame.display.init()
 
 WIDHT = 1100
 HEIGHT = 700
+
+screen = pygame.display.set_mode((WIDHT, HEIGHT))
+pygame.display.set_caption("Suri sin Speed")
+
+BLACK = (0,0,0)
 BG = pygame.transform.scale(pygame.image.load("assets\Pixel Adventure 1\Free\Background\Pink.png"),(WIDHT,HEIGHT))
 FONT = pygame.font.SysFont("arial", 30)
+
+SURI_CHAR_DOUBLE_JUMP = pygame.image.load("assets\Pixel Adventure 1\Free\Main Characters\Pink Man\Double Jump (32x32).png").convert_alpha()
+SURI_CHAR_FALL = pygame.image.load("assets\Pixel Adventure 1\Free\Main Characters\Pink Man\Fall (32x32).png").convert_alpha()
+SURI_CHAR_HIT = pygame.image.load("assets\Pixel Adventure 1\Free\Main Characters\Pink Man\Hit (32x32).png").convert_alpha()
+SURI_CHAR_IDLE = pygame.image.load("assets\Pixel Adventure 1\Free\Main Characters\Pink Man\Idle (32x32).png").convert_alpha()
+SURI_CHAR_JUMP = pygame.image.load("assets\Pixel Adventure 1\Free\Main Characters\Pink Man\Jump (32x32).png").convert_alpha()
+SURI_CHAR_RUN = pygame.image.load("assets\Pixel Adventure 1\Free\Main Characters\Pink Man\Run (32x32).png").convert_alpha()
+SURI_CHAR_WALL_JUMP = pygame.image.load("assets\Pixel Adventure 1\Free\Main Characters\Pink Man\Wall Jump (32x32).png").convert_alpha()
 
 SURI_WIDTH = 40
 SURI_HEIGHT = 60
 
 SURI_VEL = 5
 
-def resource_path(relative_path):
-    try:
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-    
-    return os.path.join(base_path, relative_path)
+sprite_sheet = spritesheet.SpriteSheet(SURI_CHAR_IDLE)
 
+animation_list = []
+animation_steps = 11
+last_update = pygame.time.get_ticks()
+animation_cooldown = 500
+frame = 0
 
-pygame.init()
-screen = pygame.display.set_mode((WIDHT, HEIGHT))
-pygame.display.set_caption("Suri sin Speed")
+for x in range(animation_steps):
+    animation_list.append(sprite_sheet.get_image(0, 32, 32, 3, BLACK))
 
-def draw(suri):
-    screen.blit(BG,(0,0))
-
-    pygame.draw.rect(screen, "red", suri)
-
-    pygame.display.update()
-    
 
 def main():
     run = True
@@ -43,9 +48,11 @@ def main():
     
     reloj = pygame.time.Clock()
 
-
     while run:
+
         reloj.tick(60)
+
+        screen.blit(animation_list[frame], (x * 72 , 0))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -57,10 +64,17 @@ def main():
             suri.x -= SURI_VEL
         if keys[pygame.K_d] and suri.x + SURI_VEL + suri.width <= WIDHT:
             suri.x += SURI_VEL
-
-        draw(suri)
+  
     
     pygame.quit()  
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    
+    return os.path.join(base_path, relative_path)
 
 if __name__ == "__main__":
     main()
