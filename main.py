28cfa606d8ca54d2b9d3df22520aa2b32ab0deb8
Gricel Diaz Zabala <griceldiazzabala@gmail.com>
2025-09-23 16:24:26 -0300

    primer commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+import sys
+import os
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    
+    return os.path.join(base_path, relative_path)
+
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit() 
+    
+    pygame.display.update()  
