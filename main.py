19a4067847ad5f9bcb06ea00e0ddb3a71016cdf1
Gricel Diaz Zabala <griceldiazzabala@gmail.com>
2025-09-24 12:02:18 -0300

    cambios radicales

--- main.py
+++ main.py
@@ -1,80 +1,61 @@
 import pygame
-pygame.init()
+from sprites import *
+from config import *
 import sys
-import os
-import spritesheet
-pygame.font.init()
-pygame.display.init()
-
-WIDHT = 1100
-HEIGHT = 700
-
-screen = pygame.display.set_mode((WIDHT, HEIGHT))
-pygame.display.set_caption("Suri sin Speed")
-
-BLACK = (0,0,0)
-BG = pygame.transform.scale(pygame.image.load("assets\Pixel Adventure 1\Free\Background\Pink.png"),(WIDHT,HEIGHT))
-FONT = pygame.font.SysFont("arial", 30)
-
-SURI_CHAR_DOUBLE_JUMP = pygame.image.load("assets\Pixel Adventure 1\Free\Main Characters\Pink Man\Double Jump (32x32).png").convert_alpha()
-SURI_CHAR_FALL = pygame.image.load("assets\Pixel Adventure 1\Free\Main Characters\Pink Man\Fall (32x32).png").convert_alpha()
-SURI_CHAR_HIT = pygame.image.load("assets\Pixel Adventure 1\Free\Main Characters\Pink Man\Hit (32x32).png").convert_alpha()
-SURI_CHAR_IDLE = pygame.image.load("assets\Pixel Adventure 1\Free\Main Characters\Pink Man\Idle (32x32).png").convert_alpha()
-SURI_CHAR_JUMP = pygame.image.load("assets\Pixel Adventure 1\Free\Main Characters\Pink Man\Jump (32x32).png").convert_alpha()
-SURI_CHAR_RUN = pygame.image.load("assets\Pixel Adventure 1\Free\Main Characters\Pink Man\Run (32x32).png").convert_alpha()
-SURI_CHAR_WALL_JUMP = pygame.image.load("assets\Pixel Adventure 1\Free\Main Characters\Pink Man\Wall Jump (32x32).png").convert_alpha()
-
-SURI_WIDTH = 40
-SURI_HEIGHT = 60
-
-SURI_VEL = 5
-
-sprite_sheet = spritesheet.SpriteSheet(SURI_CHAR_IDLE)
-
-animation_list = []
-animation_steps = 11
-last_update = pygame.time.get_ticks()
-animation_cooldown = 500
-frame = 0
-
-for x in range(animation_steps):
-    animation_list.append(sprite_sheet.get_image(0, 32, 32, 3, BLACK))
 
 
-def main():
-    run = True
+class Game:
 
-    suri = pygame.Rect(200, HEIGHT - SURI_HEIGHT, SURI_WIDTH, SURI_HEIGHT)
-    
-    reloj = pygame.time.Clock()
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
 
-    while run:
+    def new(self):
+        self.playing = True
 
-        reloj.tick(60)
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.blocks = pygame.sprite.LayeredUpdates()
+        self.enemies = pygame.sprite.LayeredUpdates()
+        self.attacks = pygame.sprite.LayeredUpdates()
 
-        screen.blit(animation_list[frame], (x * 72 , 0))
+        self.player = Player(self, 1, 2)
 
+    def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
-                break
+                self.playing = False
+                self.running = False
 
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_a] and suri.x - SURI_VEL >= 0:
-            suri.x -= SURI_VEL
-        if keys[pygame.K_d] and suri.x + SURI_VEL + suri.width <= WIDHT:
-            suri.x += SURI_VEL
-  
+    def update(self):
+        self.all_sprites.update()
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        self.clock.tick(FPS)
+        pygame.display.update()
+
+    def main(self):
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        self.running = False
     
-    pygame.quit()  
+    def game_over(self):
+        pass
 
-def resource_path(relative_path):
-    try:
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-    
-    return os.path.join(base_path, relative_path)
+    def intro_screen(self):
+        pass
 
-if __name__ == "__main__":
-    main()
+g = Game()
+g.intro_screen()
+g.new()
+while g.running:
+    g.main()
+    g.game_over()
+
+pygame.quit()
+sys.exit()
