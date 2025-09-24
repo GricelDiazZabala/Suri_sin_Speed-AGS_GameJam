401ebdbee7eafcdc3160a084e339d2a7c5a1f526
Gricel Diaz Zabala <griceldiazzabala@gmail.com>
2025-09-24 19:27:59 -0300

    base lista

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.running = True
+        self.font = pygame.font.SysFont('Arial', 30, bold=True, italic=False)
 
         self.character_spritesheet = Spritesheet('assets\suri\suri_sprite_sheet.png')
         self.terrain_spritesheet = Spritesheet('assets\Pixel Art Top Down - Basic v1.2.2\Texture\TX Tileset Grass.png')
+        self.intro_background = pygame.image.load('assets/otros/bg_provisorio.jpg')
+        self.go_background = pygame.image.load('assets/otros/bg_provisorio.jpg')
+
 
     def createTileMap(self):
         for i, row in enumerate(tilemap):
@@ -55,13 +59,60 @@ class Game:
             self.events()
             self.update()
             self.draw()
-        self.running = False
     
     def game_over(self):
-        pass
+        text = self.font.render('game over', True, RED)
+        text_rect = text.get_rect(center=(SCREEN_WIDHT/2, SCREEN_HEIGHT/2))
+
+        restart_button = Button(10, SCREEN_HEIGHT - 60, 120, 50, WHITE, BLACK, 'Restart', 32)
+
+        for sprite in self.all_sprites:
+            sprite.kill()
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
+
+            if restart_button.is_pressed(mouse_pos, mouse_pressed):
+                self.new()
+                self.main()
+
+            self.screen.blit(self.go_background, (0,0))
+            self.sceen.blit(text, text_rect)
+            self.screen.blit(restart_button.image, restart_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
 
     def intro_screen(self):
-        pass
+        intro = True
+
+        title = self.font.render('suri sin speed', True, BLACK)
+        title_rect = title.get_rect(x=10, y=10)
+
+        play_button = Button(10, 50, 100, 50, RED, BLACK, 'Play', 32)
+
+        while intro:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    intro = False
+                    self.running = False
+
+                mouse_pos = pygame.mouse.get_pos()
+                mouse_pressed = pygame.mouse.get_pressed()
+            
+            if play_button.is_pressed(mouse_pos, mouse_pressed):
+                intro = False
+            
+            self.screen.blit(self.intro_background, (0,0))
+            self.screen.blit(title, title_rect)
+            self.screen.blit(play_button.image, play_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
+
 
 g = Game()
 g.intro_screen()
